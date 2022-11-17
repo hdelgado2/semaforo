1938c5650efc1720df832de9f0cf18b95a2522bd
hendri <hendrixdelgado725@gmail.com>
2022-11-17 01:57:42 +0100

    .

--- resources/js/view/Seguridad/agregarPermisos.vue
+++ resources/js/view/Seguridad/agregarPermisos.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="container">
     <div class="card card-primary">
               <div class="card-header">
                 <h3 class="card-title">Permisos a {{form.nombre}}</h3>
@@ -9,32 +10,47 @@
                     <label for="exampleInputEmail1">Login</label>
                     <input type="email" disabled v-model="form.login" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
                   </div>
-                <div class="col-sm-6">
+                  <div class="row">
+                    <div class="col-sm-12">
                       <!-- Select multiple-->
                       <div class="form-group">
                         <label>Select Multiple</label>
                         <select multiple="" @change="cambioPermisos" class="form-control">
                           <option :value="menus.id" v-for="menus in menu" :key="menus.id">{{menus.nombre_menu}}</option>
-
                         </select>
                       </div>
                     </div>
-                    <div class="col-sm-6">
+                  </div>
+                
+                    <div class="col-sm-12">
                       <!-- Select multiple-->
                       <div class="form-group">
                         <label>Select Multiple</label>
-                        <select multiple="" @change="cambioPermisos" class="form-control">
+                        <select multiple="" @change="nivel" class="form-control">
                           <option :value="menus.id" v-for="menus in form.permisos" :key="menus.id">{{menus.modulo}}</option>
-
                         </select>
                       </div>
                     </div>
+                    <div class="row">
+                    <div class="col-sm-6">
+                      <!-- checkbox -->
+                      <div class="form-group" v-show="ocultar">
+                        <div v-for="(niveles2,index) in form.niveles" :key="index" class="custom-control custom-checkbox">
+                          <input class="custom-control-input" type="checkbox" :id="'customCheckbox'+index" >
+                          <label :for="'customCheckbox'+index" class="custom-control-label">{{niveles[index]}}</label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div class="card-footer">
                   <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
               </form>
             </div>
+
+  </div>
+   
     
 </template>
 <script>
@@ -46,9 +62,18 @@ export default {
                 'nombre':"",
                 'login': "",
                 'permisos':[],
-                'niveles' :[]
+                'niveles' :[{'Ver contenido':false},
+                            {'Crear Contenido':false},
+                            {'Editar Contenido':false},
+                            {'Borrar Contenido':false}
+                           ]                           
+                            
+                            
             }),
-            menu:{}
+            menu:{},
+            niveles:['Ver contenido','Crear Contenido','Editar Contenido','Borrar Contenido'],
+            ocultar:false
+
         }
     },mounted(){
         console.log(router.history.current.params.id)
@@ -68,12 +93,20 @@ export default {
            //busca si hay modulo añadido
             let findd = this.form.permisos.find(({id}) => (id === parseInt($event.target.value)))
               //si lo encuentra no lo deja ingresar
-              if(findd){
-                return ;
-              }
+              if(findd) return ;
         
             this.menu.map(({id,nombre_menu}) => (id === parseInt($event.target.value)) ? this.form.permisos = [...this.form.permisos,{'id':id,'modulo':nombre_menu}] : '')
-        }
+            
+        
+          },nivel($event){
+              this.ocultar = true
+              /*let findd = this.form.find(({id}) => (id === parseInt($event.target.value)))
+
+              if(findd){
+
+              };*/
+          } 
+
     }
 }
 </script>
