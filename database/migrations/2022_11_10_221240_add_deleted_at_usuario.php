d904644807a68714da3f6380d7310e7bf5c517d5
angel <thenewstart0111@gmail.com>
2023-02-14 09:06:52 -0400

    instalacion de paquetes

--- database/migrations/2022_11_10_221240_add_deleted_at_usuario.php
+++ database/migrations/2022_11_10_221240_add_deleted_at_usuario.php
@@ -14,7 +14,7 @@ class AddDeletedAtUsuario extends Migration
     public function up()
     {
         Schema::table('usuarios', function (Blueprint $table) {
-            
+            $table->id();
             $table->softDeletes();
         });
     }
